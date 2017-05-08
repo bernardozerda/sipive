9d72d3adb8691bcd0ca54a688a5c4b027422a464
bernardozerda <bzerdar@habitatbogota.gov.co>
2017-05-08 17:21:13 -0500

    fixed untracked files

--- librerias/smarty/templates_c/%%1C^1C6^1C6FDDEA%%previewReportador.tpl.php
+++
@@ -1,6 +0,0 @@
-<?php /* Smarty version 2.6.26, created on 2017-05-05 12:09:20
-         compiled from reportes/previewReportador.tpl */ ?>
-<div style="display:none" id="objReporteador"><?php echo $this->_tpl_vars['txtJsReporteador']; ?>
-</div>
-<div id="Reporteador" style=" height:480px; "/>
-<div id="objReporteadorGenerar"></div>

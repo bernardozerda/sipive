b81a9ce24b59e3ff4525aa7ba77865372d0ddad0
Bernardo Zerda <bernardo.zerda@gmail.com>
2017-05-10 11:42:50 -0500

    Correccion de errores de paso de estados en inscripcion

--- contenidos/subsidios/tipoSolucion.php
+++ contenidos/subsidios/tipoSolucion.php
@@ -3,10 +3,10 @@
 	$txtPrefijoRuta = "../../";
 	
 	include( $txtPrefijoRuta . "recursos/archivos/verificarSesion.php" );
-   include( $txtPrefijoRuta . "recursos/archivos/lecturaConfiguracion.php" );
-   include( $txtPrefijoRuta . $arrConfiguracion['librerias']['funciones'] . "funciones.php" );
-   include( $txtPrefijoRuta . $arrConfiguracion['carpetas']['recursos'] . "archivos/coneccionBaseDatos.php" );
-	
+	include( $txtPrefijoRuta . "recursos/archivos/lecturaConfiguracion.php" );
+	include( $txtPrefijoRuta . $arrConfiguracion['librerias']['funciones'] . "funciones.php" );
+	include( $txtPrefijoRuta . $arrConfiguracion['carpetas']['recursos'] . "archivos/coneccionBaseDatos.php" );
+
 	$txtValidacion = "";
 	if( $_POST['modalidad'] == 1 ){
 		$txtValidacion = " AND seqSolucion <> 1"; 
@@ -27,21 +27,19 @@
 	while( $objRes->fields ){
 		$arrSolucion[ $objRes->fields['seqSolucion'] ] = $objRes->fields['txtSolucion'];
 		$objRes->MoveNext();
-	}	
-	
-	$txtNinguna = "<option value='1'>NINGUNA</option>"; 
-	echo "
-		<select	onFocus=\"this.style.backgroundColor = '#ADD8E6';\" 
-				onBlur=\"this.style.backgroundColor = '#FFFFFF';\" 
-				name=\"seqSolucion\" 
-				id=\"seqSolucion\" 
-				style=\"width:100%;\"
-            onChange=\"asignarValorSubsidio( this , 'bolDesplazado' );\"
-		>$txtNinguna
-	";
-	foreach( $arrSolucion as $seqSolucion => $txtSolucion ){
-		echo "<option value='$seqSolucion'>$txtSolucion</option>";
+	}
+
+	echo "<select onFocus=\"this.style.backgroundColor = '#ADD8E6';\"";
+	echo " onBlur=\"this . style . backgroundColor = '#FFFFFF';\"";
+	echo " name=\"seqSolucion\"";
+	echo " id=\"seqSolucion\"";
+	echo " style=\"width:100%;\"";
+	echo " onChange=\"asignarValorSubsidio( this , 'bolDesplazado' );\"";
+	echo "><option value='1'>NINGUNA</option>";
+	$txtSeleccionado = (count($arrSolucion) == 1)? "selected" : "";
+	foreach($arrSolucion as $seqSolucion => $txtSolucion){
+		echo "<option value='$seqSolucion' $txtSeleccionado>$txtSolucion</option>";
 	}
 	echo "</select>";
-	
+
 ?>

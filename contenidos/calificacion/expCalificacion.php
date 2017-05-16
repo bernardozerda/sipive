d58d7945049f99f3ec93141b042485692a71a76d
lbastoz <lbastoz@PCCOM-65767-P2.habitatbogota.gov.co>
2017-05-16 15:16:53 -0500

    Cambios de actulizacion de los recursos de los ciudadanos

--- contenidos/calificacion/expCalificacion.php
+++ contenidos/calificacion/expCalificacion.php
@@ -5,45 +5,92 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+$txtPrefijoRuta = "../../librerias/";
 require_once '../../librerias/phpExcel/Classes/PHPExcel.php';
 require_once '../../librerias/phpExcel/Classes/PHPExcel/Writer/Excel2007.php';
-
+include( $txtPrefijoRuta. "clases/calificacion.class.php" );
 $objPHPExcel = new PHPExcel();
+$claCalificacion = new calificacion();
 
 $conexion = mysql_connect("localhost", "sdht_usuario", "Ochochar*1");
 mysql_set_charset('utf8', $conexion);
 mysql_select_db("sdht_sipive", $conexion);
 
-
-$sql = "select seqFormulario, numDocumento,
-concat(txtNombre1,' ', txtNombre2, ' ', txtApellido1, ' ', txtApellido2) as postulante, 
-cal.infHogar, cal.cantMiembrosHogar, cal.totalIngresos, sum(op.total)
-from t_frm_calificacion_plan3  cal
-left join t_frm_calificacion_operaciones op using(seqCalificacion)
-left join t_frm_formulario using(seqFormulario)
-left join t_frm_hogar hog using(seqFormulario)
-left join t_ciu_ciudadano ciu using (seqCiudadano)
-where fchCalificacion like '" . $_GET['fchCal'] . "'
-and seqParentesco = 1
-group by seqFormulario";
+$fecha = $_GET['fchCal'];
+$sql = $claCalificacion->obtenerResumenCalificacion($fecha);
+//echo $sql;
+//die();
 
 $resultdl = mysql_query($sql, $conexion) or die(mysql_error());
 $registros = mysql_num_rows($resultdl);
 
 
 $tituloReporte = Array();
-$tituloReporte[0] = "ID Hogar";
-$tituloReporte[1] = "Documento";
-$tituloReporte[2] = "Postulante Principal";
-$tituloReporte[3] = "Inf del Hogar";
-$tituloReporte[4] = "Integrantes";
-$tituloReporte[5] = "Ingresos";
-$tituloReporte[6] = "Total";
-
+$tituloReporte[] = "ID Hogar";
+$tituloReporte[] = "Documento";
+$tituloReporte[] = "Postulante Principal";
+$tituloReporte[] = "Telefono 1";
+$tituloReporte[] = "Telefono 2";
+$tituloReporte[] = "Celular";
+$tituloReporte[] = "Tipo\nVictima";
+$tituloReporte[] = "Modalidad";
+$tituloReporte[] = "Integrantes";
+$tituloReporte[] = "miembros > 15";
+$tituloReporte[] = "Años\nAprobados";
+$tituloReporte[] = "Calculo\nEducacion";
+$tituloReporte[] = "Dicotomia\nEducacion";
+$tituloReporte[] = "Total\nEducacion";
+$tituloReporte[] = "Miembros\nRegimen\nSubsidiado";
+$tituloReporte[] = "Calculo Miembros\nRegimen\nSubsidiado";
+$tituloReporte[] = "Total Miembros\nRegimen\nSubsidiado";
+$tituloReporte[] = "Cohabitacion";
+$tituloReporte[] = "Dicotomia\ncohabitación";
+$tituloReporte[] = "Total\nCohabitación";
+$tituloReporte[] = "Dormitorios";
+$tituloReporte[] = "Calculo\nHacinamiento";
+$tituloReporte[] = "Total\nHacinamiento";
+$tituloReporte[] = "Ingresos\nHogar";
+$tituloReporte[] = "Calculo\nIngresos";
+$tituloReporte[] = "Total\nIngresos";
+$tituloReporte[] = "Miembros\nOcupados";
+$tituloReporte[] = "Calculo\nDependencia\nEconomica";
+$tituloReporte[] = "Años Aprobados\nJefe Hogar";
+$tituloReporte[] = "Dicotomia\nDependencia\nEconomica";
+$tituloReporte[] = "Total\nDependencia\nEconomica";
+$tituloReporte[] = "Cantidad\n<= 12 años";
+$tituloReporte[] = "Calculo\nMenores";
+$tituloReporte[] = "Total\nMenores";
+$tituloReporte[] = "Cantidad\nHijos";
+$tituloReporte[] = "Mujer\nCabeza\nHogar";
+$tituloReporte[] = "Persona\nConyugue";
+$tituloReporte[] = "Dicotomia\nMujer\nCabeza Hogar";
+$tituloReporte[] = "Total\nMujer\nCabeza Hogar";
+$tituloReporte[] = "Cantidad\n >=60 Años";
+$tituloReporte[] = "Calculo\nHogar con\nAdulto";
+$tituloReporte[] = "Total\nHogar con\nAdulto";
+$tituloReporte[] = "Cantidad\n Cond. Especial";
+$tituloReporte[] = "Calculo\nCond Especial";
+$tituloReporte[] = "Total\nCond Especial";
+$tituloReporte[] = "Cantidad\nGrupo Etnico";
+$tituloReporte[] = "Calculo\nGrupo Etnico";
+$tituloReporte[] = "Total\nGrupo Etnico";
+$tituloReporte[] = "Cantidad\nAdolecentes";
+$tituloReporte[] = "Calculo\nAdolecentes";
+$tituloReporte[] = "Total\nAdolecentes";
+$tituloReporte[] = "Hombre\nCabeza\nHogar";
+$tituloReporte[] = "Persona\nConyugue";
+$tituloReporte[] = "Dicotomia\nHombre\nCabeza Hogar";
+$tituloReporte[] = "Total\nHombre\nCabeza Hogar";
+$tituloReporte[] = "Cantidad\nLGTBI";
+$tituloReporte[] = "Calculo\nLGTBI";
+$tituloReporte[] = "Total\nLGTBI";
+$tituloReporte[] = "Dicotomia\nPrograma";
+$tituloReporte[] = "Total\nPrograma";
+$tituloReporte[] = "Total";
 
 $arrNomCol = array("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "AA", "AB", "AC",
-    "AD", "AE", "AF", "AG", "AH", "AI", "AJ", "AK", "AL", "AM", "AN", "AO", "AP", "AQ", "AR", "AS", "AT", "AU", "AV", "AW", "AX", "AY", "AZ");
+    "AD", "AE", "AF", "AG", "AH", "AI", "AJ", "AK", "AL", "AM", "AN", "AO", "AP", "AQ", "AR", "AS", "AT", "AU", "AV", "AW", "AX", "AY", "AZ", "BA", "BB", "BC",
+    "BD", "BE", "BF", "BG", "BH", "BI", "BJ");
 
 $style_header = array(
     'fill' => array(
@@ -99,7 +146,7 @@ if ($registros > 0) {
     $docConsult = Array();
 
     $tiltle = 0;
-    $titulos = 7;
+    $titulos = 61;
     $field = 0;
     while ($field !== $titulos) {
         $objPHPExcel->setActiveSheetIndex(0)->SetCellValue($arrNomCol[$field] . "1", $tituloReporte[$tiltle])->getRowDimension('1')->setRowHeight(80);
@@ -118,7 +165,7 @@ if ($registros > 0) {
         while ($field !== $titulos) {
             // $objPHPExcel->setActiveSheetIndex(0)->SetCellValue($arrNomCol[$field] . $rowcount, utf8_encode($row[$field]));
             $objPHPExcel->setActiveSheetIndex(0)->SetCellValue($arrNomCol[$field] . $rowcount, str_replace('  ', '', $row[$field]));
-            $objPHPExcel->getActiveSheet()->getStyle('A' . $rowcount . ':G' . $rowcount)->applyFromArray($styleArrayBody);
+            $objPHPExcel->getActiveSheet()->getStyle('A' . $rowcount . ':BI' . $rowcount)->applyFromArray($styleArrayBody);
             $field++;
         }
         $rowcount++;

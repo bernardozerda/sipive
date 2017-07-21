82b335b1d855b8afc09871b3afcf7981db7495c6
Bernardo Zerda <bernardo.zerda@gmail.com>
2017-07-21 11:18:18 -0500

    FIX formato impresion estudio de titulos

--- librerias/clases/CasaMano.class.php
+++ librerias/clases/CasaMano.class.php
@@ -51,7 +51,8 @@ class CasaMano
         // postulacion individual
         // proyectos de la SDHT
         $this->arrFases['pin']['modalidad'] = array(1, 6, 9, 12, 13);
-        $this->arrFases['pin']['esquema'] = array(0, 1, 4, 9);
+        //$this->arrFases['pin']['esquema'] = array(0, 1, 4, 9);
+        $this->arrFases['pin']['esquema'] = array(0,1,2,3,4,6,7,8,9);
 
         $this->arrFases['pin']['postulacion']['grupos'] = array(6, 7, 8);
         $this->arrFases['pin']['postulacion']['permitido']['etapas'] = array(2,3,4,5);

1e4abc006966562667051765929f0a7d042d0921
Bernardo Zerda <bernardo.zerda@gmail.com>
2017-10-17 14:53:13 -0500

    ADD: Archivo para test de resultados de variables de calificacion encuestas

---
+++ test.php
@@ -0,0 +1,21 @@
+<?php
+
+$txtPrefijoRuta = "./";
+include( $txtPrefijoRuta . "recursos/archivos/verificarSesion.php" );
+include( $txtPrefijoRuta . "recursos/archivos/lecturaConfiguracion.php" );
+include( $txtPrefijoRuta . $arrConfiguracion['librerias']['funciones'] . "funciones.php" );
+include( $txtPrefijoRuta . $arrConfiguracion['carpetas']['recursos'] . "archivos/inclusionSmarty.php" );
+include( $txtPrefijoRuta . $arrConfiguracion['carpetas']['recursos'] . "archivos/coneccionBaseDatos.php" );
+include( $txtPrefijoRuta . $arrConfiguracion['librerias']['clases'] . "FormularioSubsidios.class.php" );
+include( $txtPrefijoRuta . $arrConfiguracion['librerias']['clases'] . "Ciudadano.class.php" );
+include( $txtPrefijoRuta . $arrConfiguracion['librerias']['clases'] . "Encuestas.class.php" );
+
+$documento = 1123567000;
+
+$encuesta = new Encuestas();
+
+$calificacion = $encuesta->obtenerVariablesCalificacion($documento);
+
+pr($calificacion);
+
+?>

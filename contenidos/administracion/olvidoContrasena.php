d5fe075e01e6d77c4433f195e680e9c08c9d5fe2
bernardozerda <bernardo.zerda@gmail.com>
2018-01-23 11:42:54 -0500

    FIX: Reporte Asignados Actos Administrativos sacaba la unidad de t_frm_formulario, la saca ahora de t_aad_formulario_acto

--- contenidos/administracion/olvidoContrasena.php
+++ contenidos/administracion/olvidoContrasena.php
@@ -19,9 +19,6 @@ include( $txtPrefijoRuta . $arrConfiguracion['librerias']['clases'] . "Autentica
 include( $txtPrefijoRuta . $arrConfiguracion['librerias']['phpmailer'] . "class.phpmailer.php" );
 include( $txtPrefijoRuta . $arrConfiguracion['librerias']['nusoap'] . "nusoap.php" );
 
-
-
-
 $arrErrores = array(); // donde se almacenan los errores
 // Esta expresion regular valida que el correo electronico escrito por el usuario
 // tenga una sintaxis correcta, si falla la sintaxis entra al error
@@ -88,49 +85,111 @@ if (empty($arrErrores)) {
              */
             $txtSubject = "Solicitud de Cambio de Clave";
 
+//            $txtMensajeHtml = "
+//                    <center>
+//                    <table cellspacing='5'
+//                           cellpadding='5'
+//                           width='500px'
+//                           bgcolor='#ECECEC'
+//                           style='
+//                             padding:5px;
+//                             font-family: Verdana, Geneva, Arial, Helvetica,
+//                             sans-serif; color:black:
+//                             font-size:11px;
+//                             font-weight: bold;
+//                           '
+//                    >
+//                        <tr><td align='justify' colspan='2' style='padding:10px; color:#4682b4; font-size:14px; border: 1px dotted #999999; ' bgcolor='#FFFFFF'>
+//                            <b>Hola " . $arrUsuario[$seqUsuario]->txtNombre . " " . $arrUsuario[$seqUsuario]->txtApellido . "</b>
+//						</td></tr>
+//						<tr><td colspan='2' bgcolor='#F9F9F9' style='padding:10px;'>
+//                            Has usado el procedimiento para recuperar tu contrase&ntilde;a para ingresar al SDHT - Subsidios de vivienda, te informamos que
+//                            tu clave ha sido restaurada, tu clave actual es: <i>(recuerda respetar mayusculas y minusculas)</i>
+//						</td></tr>
+//						<tr><td colspan='2' height='40px' bgcolor='#FDF5E6' style='color:#C8211B; font-size:14px;' align='center'>
+//							$txtNuevaClave
+//						</td></tr>
+//						<tr><td colspan='2' bgcolor='#F9F9F9' style='padding:10px;'>
+//							Por tu seguridad, la pr&oacute;xima vez que ingreses
+//                            el sistema te solicitar&aacute; que cambies tu contrase&ntilde;a.
+//                        </td></tr>
+//                        <tr>
+//							<td style='padding:5px' width='30px' align='center'>
+//								<img src='http://sdv.habitatbogota.gov.co/sipive/recursos/imagenes/escudo_bogota.png'>
+//							</td>
+//							<td style='padding:10px'>
+//	                            Secretar&iacute;a Distrital del H&aacute;bitat<br>
+//	                            Bogot&aacute; D.C. - Colombia<br>
+//	                        </td>
+//						</tr>
+//                    </table>
+//                    </center>
+//                ";
+
             $txtMensajeHtml = "
-                    <center>
-                    <table cellspacing='5' cellpadding='5' width='500px' bgcolor='#ECECEC' style='padding:5px; font-family: Verdana, Geneva, Arial, Helvetica, sans-serif; color:black: font-size:11px; font-weight: bold;'>
-                        <tr><td align='justify' colspan='2' style='padding:10px; color:#4682b4; font-size:14px; border: 1px dotted #999999; ' bgcolor='#FFFFFF'>
-                            <b>Hola " . $arrUsuario[$seqUsuario]->txtNombre . " " . $arrUsuario[$seqUsuario]->txtApellido . "</b>
-						</td></tr>
-						<tr><td colspan='2' bgcolor='#F9F9F9' style='padding:10px;'>
-                            Has usado el procedimiento para recuperar tu contrase&ntilde;a para ingresar al SDHT - Subsidios de vivienda, te informamos que
-                            tu clave ha sido restaurada, tu clave actual es: <i>(recuerda respetar mayusculas y minusculas)</i>
-						</td></tr>
-						<tr><td colspan='2' height='40px' bgcolor='#FDF5E6' style='color:#C8211B; font-size:14px;' align='center'>
-							$txtNuevaClave
-						</td></tr>
-						<tr><td colspan='2' bgcolor='#F9F9F9' style='padding:10px;'>	
-							Por tu seguridad, la pr&oacute;xima vez que ingreses
-                            el sistema te solicitar&aacute; que cambies tu contrase&ntilde;a.
-                        </td></tr>
+                <center>
+                    <table cellspacing='0'
+                           cellpadding='20'
+                           width='40%'
+                           bgcolor='#FFFFFF'
+                           style='
+                             padding:5px;
+                             font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
+                           '
+                    >
                         <tr>
-							<td style='padding:5px' width='30px' align='center'>
-								<img src='http://sdv.habitatbogota.gov.co/sipive/recursos/imagenes/escudo_bogota.png'>
-							</td>
-							<td style='padding:10px'>
-	                            Secretar&iacute;a Distrital del H&aacute;bitat<br>
-	                            Bogot&aacute; D.C. - Colombia<br>
-	                        </td>
-						</tr>
+                            <td width='20%'
+                                align='center'
+                                style='border-bottom: 1px solid #c4c4c4;'
+                            >
+                                <img src='http://sdv.habitatbogota.gov.co/sipive/recursos/imagenes/bogota.jpg'>
+                            </td>
+                            <td align='center'
+                                style='
+                                    color:#666666;
+                                    border-bottom: 1px solid #c4c4c4;
+                                    font-size: 20px;
+                                    padding-top: 15px;
+                                    padding-bottom: 15px;
+                
+                                '
+                            >
+                                <p><strong>SISTEMA DE INFORMACIÓN DEL<br>PROGRAMA INTEGRAL DE VIVIENDA EFECTVA</strong></p>
+                                <span style='font-size: 14px'>
+                                    Secretar&iacute;a Distrital de H&aacute;bitat<br>
+                                    Bogot&aacute; D.C. - Colombia
+                                </span>
+                            </td>                            
+                        </tr>
+                        <tr>
+                            <td colspan='2' style='padding:40px;'>
+                                <p>
+                                    Hola " . $arrUsuario[$seqUsuario]->txtNombre . " " . $arrUsuario[$seqUsuario]->txtApellido . "
+                                </p>
+                                <p>
+                                    Ha recibido este mensaje porque ha solicitado iniciar el procedimiento de recuperación de contraseñas.
+                                    En este momento su contraseña ha sido restablecida y le solicitarémos cambiarla al momento de ingresar.
+                                </p>
+                                La clave que ha sido generada para su usuario es:
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colspan='2' height='40px' bgcolor='#D4EDDA' style='color:##155724; font-size:18px;' align='center'>
+                                $txtNuevaClave<br>
+                                <span style='font-size: 12px'>
+                                    <i>(Recuerde respetar mayusculas y minusculas)</i>
+                                </span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colspan='2' style='padding: 40px'>
+                                Cordial saludo:<br>Equipo de Sistemas
+                            </td>
+                        </tr>
                     </table>
-                    </center>
-                ";
-            //$txtMensajeHtml = mb_eregi_replace("[\]", '', $txtMensajeHtml);
-
-
-            // Este mensaje se usa si el cliente de correo del usuario no soporta html
-            $txtMensajeTexto = "
-                    Hola " . $arrUsuario[$seqUsuario]->txtNombre . " " . $arrUsuario[$seqUsuario]->txtApellido . ": \r\n\r\n
-                    Has usado el procedimiento para recuperar tu contrase&ntilde;a para ingresar al SDHT - Subsidios de vivienda, te informamos que \r\n
-                    tu clave ha sido restaurada, tu calve actual es $txtNuevaClave, por tu seguridad, la proxima vez que ingreses \r\n
-                    el sistema te solicitar&aacute; que cambies tu contrase&ntilde;a.\r\n\r\n				
-					
-                    Secretar&iacute;a Distrital del H&aacute;bitat.\r\n\r\n
-                    Bogot&aacute; D.C. - Colombia\r\n                    
-                ";
-
+                </center>     
+            ";
+            
             $servidor = $arrConfiguracion['correo']['servidor'];
             $puerto = $arrConfiguracion['correo']['puerto'];
             $seguridad = $arrConfiguracion['correo']['seguridad'];
@@ -143,14 +202,15 @@ if (empty($arrErrores)) {
             $mail->IsSMTP();
             $mail->SMTPAuth = true;
             $mail->SMTPSecure = $seguridad;
+            $mail->SMTPDebug = 0;
             $mail->Host = $servidor;
             $mail->Port = $puerto;
             $mail->Username = $usuario;
             $mail->Password = $clave;
-            $mail->From = "bzerdar@habitatbogota.gov.co";
+            $mail->From = $usuario;
             $mail->FromName = $nombre;
             $mail->Subject = $txtSubject;
-            $mail->Body = $txtMensajeHtml;
+            $mail->Body = utf8_decode($txtMensajeHtml);
             $mail->AddAddress($txtCorreo, $nombrecompleto);
             $mail->IsHTML(true);
             if ($mail->Send()) {

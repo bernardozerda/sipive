8be708474947101bb65041c862debdafa9f43c77
bernardozerda <bernardo.zerda@gmail.com>
2018-03-09 17:21:08 -0500

    ADD: Completar etiquetas HTTPS en generacion de links de impresion

--- contenidos/migracionesIndividual/masivoPlantillaEscrituracion/insertar.php
+++ contenidos/migracionesIndividual/masivoPlantillaEscrituracion/insertar.php
@@ -652,7 +652,7 @@ function generarExcel($arreglo) {
                     <td><?= $arreglo['numDocumento'][$int] ?></td>
                     <td><?= $arreglo['seqDesembolso'][$int] ?></td>
                     <td><?= $arreglo['seqFormulario'][$int] ?></td>
-                    <td><a href="https://<?= $_SERVER['HTTP_HOST'] ?>/sipive/contenidos/desembolso/formatoBusquedaOferta.php?seqCasaMano=0&bolEscrituracion=1&seqFormulario=<?= $arreglo['seqFormulario'][$int] ?>">http://<?= $_SERVER['HTTP_HOST'] ?>/sipive/contenidos/desembolso/formatoBusquedaOferta.php?seqCasaMano=0&bolEscrituracion=1&seqFormulario=<?= $arreglo['seqFormulario'][$int] ?></a></td>
+                    <td><a href="https://<?= $_SERVER['HTTP_HOST'] ?>/sipive/contenidos/desembolso/formatoBusquedaOferta.php?seqCasaMano=0&bolEscrituracion=1&seqFormulario=<?= $arreglo['seqFormulario'][$int] ?>">https://<?= $_SERVER['HTTP_HOST'] ?>/sipive/contenidos/desembolso/formatoBusquedaOferta.php?seqCasaMano=0&bolEscrituracion=1&seqFormulario=<?= $arreglo['seqFormulario'][$int] ?></a></td>
                 </tr>
 
                 <?php

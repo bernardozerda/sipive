51965d5ed7cd92e8bec148fd62f60e0d7e21a997
lbastoz <lbastoz@habitatbogota.gov.co>
2018-06-19 12:51:19 -0500

    ficha

--- contenidos/proyectos/contenidos/datosSeguimientoFicha.php
+++ contenidos/proyectos/contenidos/datosSeguimientoFicha.php
@@ -6,12 +6,15 @@ include( $txtPrefijoRuta . "recursos/archivos/verificarSesion.php" );
 include( $txtPrefijoRuta . "recursos/archivos/lecturaConfiguracion.php" );
 include( $txtPrefijoRuta . $arrConfiguracion['librerias']['funciones'] . "funciones.php" );
 
-
 include( $txtPrefijoRuta . $arrConfiguracion['carpetas']['recursos'] . "archivos/inclusionSmarty.php" );
 include( $txtPrefijoRuta . $arrConfiguracion['carpetas']['recursos'] . "archivos/coneccionBaseDatos.php" );
 include( $txtPrefijoRuta . $arrConfiguracion['librerias']['clases'] . "DatosGeneralesProyectos.class.php" );
 include( $txtPrefijoRuta . $arrConfiguracion['librerias']['clases'] . "SeguimientoProyectos.class.php" );
 include( $txtPrefijoRuta . $arrConfiguracion['librerias']['clases'] . "Proyecto.class.php" );
+
+
+$claDatosProy = new DatosGeneralesProyectos();
+$claProyecto = new Proyecto();
 
 $arrProyectos = array();
 $arrOferentesProy = array();
@@ -20,9 +23,9 @@ $arrConjuntoResidencial = Array();
 $arraDatosPoliza = Array();
 $arrCronogramaFecha = Array();
 $arrayFideicomitente = Array();
+$arraImagenes = Array();
 
-$claDatosProy = new DatosGeneralesProyectos();
-$claProyecto = new Proyecto();
+
 $txtPlantilla = "proyectos/vistas/listaProyectosFicha.tpl";
 $idProyecto = 0;
 $seqSeguimientoFicha = 0;
@@ -33,20 +36,38 @@ $id = $_REQUEST['id'];
 $arrProyectos[0] = 0;
 $arraSegFicha[0] = 0;
 $arrayTextos[0] = 0;
+$arraImagenes[0] = 0;
 
-if (isset($_REQUEST['seqProyecto']) || isset($_REQUEST['seqSeguimientoFicha'])) {
+if ((isset($_REQUEST['seqProyecto']) || isset($_REQUEST['seqSeguimientoFicha'])) && $id != 2) {    
+    
     $idProyecto = $_REQUEST['seqProyecto'];
     $seqSeguimientoFicha = $_REQUEST['seqSeguimientoFicha'];
+    $arraSegFicha = $claDatosProy->obtenerSeguimientosFicha($idProyecto, $seqSeguimientoFicha);
     $txtPlantilla = "proyectos/vistas/inscripcionSeguimiento.tpl";
-    
 }
 if (isset($_REQUEST['seqSeguimientoFicha'])) {
-
     $arrayTextos = $claDatosProy->obtenerlistaTextos($seqSeguimientoFicha);
 }
-$arraSegFicha = $claDatosProy->obtenerSeguimientosFicha($idProyecto, $seqSeguimientoFicha);
-$arrProyectos = $claDatosProy->obtenerlistaProyectos($idProyecto, $id);
+if ($id == 2) {
+    $idProyecto = $_REQUEST['seqProyecto'];
+    $txtPlantilla = "proyectos/vistas/listaImagenes.tpl";
+    $directorio = '../../../recursos/proyectos/proyecto-' . $idProyecto . '/imagenes';
 
+    $dir = @dir($directorio);
+     
+    $arraImagenes = Array();
+    if ($dir) {
+      
+        while (($archivo = $dir->read()) !== false) {
+            if ($archivo[0] != ".") {
+                $arraImagenes[] = 'proyecto-' . $idProyecto . '/imagenes/' . $archivo;
+                continue;
+            }
+        }
+    }
+}
+
+$arrProyectos = $claDatosProy->obtenerlistaProyectos($idProyecto, $id);
 
 
 $seqUsuario = $_SESSION['seqUsuario'];
@@ -59,6 +80,7 @@ $claSmarty->assign("seqUsuario", $_SESSION['seqUsuario']);
 $claSmarty->assign("seqProyecto", $_REQUEST['seqProyecto']);
 $claSmarty->assign("arraSegFicha", $arraSegFicha);
 $claSmarty->assign("arrayTextos", $arrayTextos);
+$claSmarty->assign("arrImagenes", $arraImagenes);
 $claSmarty->assign("page", "datosSeguimientoFicha.php?tipo=1&id=" . $id);
 
 

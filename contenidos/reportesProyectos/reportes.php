55adf878d1a75dc13727ce4bbe042045a0a84ba8
bernardozerda <bernardo.zerda@gmail.com>
2018-06-29 10:51:27 -0500

    ADD: Listado de reportes para proyectos

--- contenidos/reportesProyectos/reportes.php
+++ contenidos/reportesProyectos/reportes.php
@@ -7,7 +7,7 @@ include($txtPrefijoRuta . "recursos/archivos/lecturaConfiguracion.php");
 include( $txtPrefijoRuta . $arrConfiguracion['librerias']['funciones'] . "funciones.php" );
 include( $txtPrefijoRuta . $arrConfiguracion['carpetas']['recursos'] . "archivos/inclusionSmarty.php" );
 include( $txtPrefijoRuta . $arrConfiguracion['carpetas']['recursos'] . "archivos/coneccionBaseDatos.php" );
-include( $txtPrefijoRuta . $arrConfiguracion['librerias']['clases']   . "GestionFinancieraProyectos.class.php" );
+include( $txtPrefijoRuta . $arrConfiguracion['librerias']['clases']   . "ReportesProyectos.class.php" );
 include( $txtPrefijoRuta . $arrConfiguracion['librerias']['clases']   . "PHPExcel.php" );
 include("../../librerias/phpExcel/Classes/PHPExcel/Writer/Excel2007.php");
 include("../../librerias/phpExcel/Classes/PHPExcel/IOFactory.php");
@@ -25,7 +25,7 @@ $arrReporte['fichaTecnica']['descripcion'] = "Obtiene la información consignada
 if($_GET['reporte'] != "") {
     $txtClave = $_GET['reporte'];
     $txtReporte = $arrReporte[$txtClave]['funcion'];
-    $claGestion = new GestionFinancieraProyectos();
+    $claGestion = new Reportes();
     $claGestion->$txtReporte();
 }else{
     $claSmarty->assign("arrReporte", $arrReporte);

33e566bcc3bc229e2d2d7bb109da13f402aa2819
bernardozerda <bernardo.zerda@gmail.com>
2018-08-01 10:46:58 -0500

    FIX: Nombre de la modalidad ajustada para solicitud de desmbolsos

--- contenidos/desembolso/formatoSolicitudDesembolso.php
+++ contenidos/desembolso/formatoSolicitudDesembolso.php
@@ -128,8 +128,6 @@ $numRegistros = array_shift(array_keys($claSeguimiento->obtenerRegistros(1, 0)))
 
 $numSolicitudes = count($claDesembolso->arrSolicitud['resumen']['fechas']);
 
-
-
 $result = $aptBd->execute("select txtFlujo from T_DES_FLUJO where seqFormulario = " . $_GET['seqFormulario']);
 $txtflujo = $result->fields['txtFlujo'];
 $claSmarty->assign("Flujo", $txtflujo);
@@ -148,6 +146,7 @@ $claSmarty->assign("claFormulario", $claFormulario);
 $claSmarty->assign("txtUsuarioSesion", $_SESSION['txtNombre'] . " " . $_SESSION['txtApellido']);
 $claSmarty->assign("numRegistro", $numRegistros);
 $claSmarty->assign("numSolicitudes", $numSolicitudes);
+$claSmarty->assign("txtModalidad", ucwords(mb_ereg_replace("nueva","usada",mb_strtolower($arrModalidad[$claFormulario->seqModalidad]))));
 
 $claSmarty->display("desembolso/formatoSolicitudDesembolso.tpl");
 

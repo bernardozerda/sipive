d4ce7d16d83da105a6175f67d5057bd4d26be213
bernardozerda <bernardo.zerda@gmail.com>
2018-10-08 10:21:48 -0500

    Soluciona error al descargar el archivo

--- contenidos/inscripcionFonvivienda/descargar.php
+++ contenidos/inscripcionFonvivienda/descargar.php
@@ -7,6 +7,7 @@
 $txtPrefijoRuta = "../../";
 include( $txtPrefijoRuta . "recursos/archivos/verificarSesion.php" );
 include( $txtPrefijoRuta . "recursos/archivos/lecturaConfiguracion.php" );
+include( $txtPrefijoRuta . $arrConfiguracion['librerias']['funciones'] . "funciones.php" );
 include( $txtPrefijoRuta . $arrConfiguracion['carpetas']['recursos'] . "archivos/coneccionBaseDatos.php" );
 include( $txtPrefijoRuta . $arrConfiguracion['librerias']['clases'] . "InscripcionFonvivienda.class.php" );
 

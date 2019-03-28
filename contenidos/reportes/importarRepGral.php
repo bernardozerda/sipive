504e670e69057a7896cf0c77b71d480788ffa750
Liliana <lbastoz@habitatbogota.gov.co>
2019-03-28 13:06:20 -0500

    Commit de cambios en reporte general V2

--- contenidos/reportes/importarRepGral.php
+++ contenidos/reportes/importarRepGral.php
@@ -47,88 +47,93 @@ $valDoc = "";
             <div class="caption">
                 <?php
                 //Recorremos el bucle para leer línea por línea
-                foreach ($lineas as $linea_num => $linea) {
 
-                    $documento = 0;
-                    $sql = "SELECT * FROM t_frm_reporte_gral where ";
-                    $datos = explode("\t", $linea);
-                    //abrimos bucle
-                    /* si es diferente a 0 significa que no se encuentra en la primera línea 
-                      (con los títulos de las columnas) y por lo tanto puede leerla */
-                    if ($i == 0 && count($datos) != 103) {
-                        echo "El archivo no cumple con las columnas requeridas!! " . count($datos);
-                        die();
-                    } else if ($i > 0) {
-                        //  echo "<br><br>" . $key . " - " . $linea;
-                        // var_dump($datos);
-                        $insert = "(";
-                        foreach ($datos as $key => $value) {
-                            if ($key == 0) {
-                                $seqReporte .= $value . ",";
-                                $posicion = $value;
-                                $sql .= " seqReporteHogar = " . $value;
-                            } else if ($key == 5) {
-                                $documento = $value;
-                                $sql .= " and DocumentoCiudadano = " . $value . " and  txtNombreReporte = '" . $_POST['name'] . "'";
+                if (count($lineas) < 5000) {
+                    foreach ($lineas as $linea_num => $linea) {
+                        $documento = 0;
+                        $sql = "SELECT * FROM t_frm_reporte_gral where ";
+                        $datos = explode("\t", $linea);
+                        //abrimos bucle
+                        /* si es diferente a 0 significa que no se encuentra en la primera línea 
+                          (con los títulos de las columnas) y por lo tanto puede leerla */
+                        if ($i == 0 && count($datos) != 103) {
+                            echo "El archivo no cumple con las columnas requeridas!! " . count($datos);
+                            die();
+                        } else if ($i > 0) {
+                            //  echo "<br><br>" . $key . " - " . $linea;
+                            // var_dump($datos);
+                            $insert = "(";
+                            foreach ($datos as $key => $value) {
+                                if ($key == 0) {
+                                    $seqReporte .= $value . ",";
+                                    $posicion = $value;
+                                    $sql .= " seqReporteHogar = " . $value;
+                                } else if ($key == 5) {
+                                    $documento = $value;
+                                    $sql .= " and DocumentoCiudadano = " . $value . " and  txtNombreReporte = '" . $_POST['name'] . "'";
+                                }
+                                // echo "<br> ".$key." -> ".$value."<br>";
+                                if (count(explode("/", $value)) == 3) {//
+                                    $value = str_replace('/', '-', $value);
+                                    $value = date('Y-m-d', strtotime($value));
+                                    //$insert .= "'" . $value . "',";
+                                }
+                                $insert .= is_numeric($value) ? $value . "," : "'" . utf8_encode(trim($value)) . "',";
+                                if ($key == 102) {
+                                    $insert = substr_replace($insert, '', -1, 1);
+                                    $insert .= "),";
+                                }
                             }
-                            // echo "<br> ".$key." -> ".$value."<br>";
-                            if (count(explode("/", $value)) == 3) {//
-                                $value = str_replace('/', '-', $value);
-                                $value = date('Y-m-d', strtotime($value));
-                                //$insert .= "'" . $value . "',";
-                            }
-                            $insert .= is_numeric($value) ? $value . "," : "'" . utf8_encode(trim($value)) . "',";
-                            if ($key == 102) {
-                                $insert = substr_replace($insert, '', -1, 1);
-                                $insert .= "),";
+                            $sqlInsert .= $insert;
+                            // echo $sql."<br><br>";
+                            $validar = $claReporte->obtenerReportesGral($sql);
+                            // echo "<br>Validar - > " . $validar . "<br>";
+                            if (!$validar) {
+                                $valDoc .= "El registro " . $posicion . " con N° de documento " . $documento . " del reporte " . $_POST['name'] . " No coinciden con los previamente registrados<br>";
+                                $band = false;
                             }
                         }
-                        $sqlInsert .= $insert;
-                        // echo $sql."<br><br>";
-                        $validar = $claReporte->obtenerReportesGral($sql);
-                        // echo "<br>Validar - > " . $validar . "<br>";
-                        if (!$validar) {
-                            $valDoc .= "El registro " . $posicion . " con N° de documento " . $documento . " del reporte " . $_POST['name'] . " No coinciden con los previamente registrados<br>";
-                            $band = false;
-                        }
+                        /* Cuando pase la primera pasada se incrementará nuestro valor y a la siguiente pasada ya 
+                          entraremos en la condición, de esta manera conseguimos que no lea la primera línea. */
+                        $i++;
+                        //cerramos bucle
                     }
-                    /* Cuando pase la primera pasada se incrementará nuestro valor y a la siguiente pasada ya 
-                      entraremos en la condición, de esta manera conseguimos que no lea la primera línea. */
-                    $i++;
-                    //cerramos bucle
-                }
-
-                //echo "<br>band *** " . $band;
 
 
-                if ($band) {
+                    //echo "<br>band *** " . $band;
 
-                    $seqReporte = substr_replace($seqReporte, '', -1, 1);
-                    $sqlDelete .= $seqReporte . ");";
+
+                    if ($band) {
+
+                        $seqReporte = substr_replace($seqReporte, '', -1, 1);
+                        $sqlDelete .= $seqReporte . ");";
 //                    echo "<p>**" . $sqlDelete . " </p><br>";
 //                    
 //                    echo "<br><p>".$sqlInsert."</p>";
 //                    die();
 
-                    $deleteVal = $claReporte->obtenerReportesGral($sqlDelete);
-                    if ($deleteVal) {
-                        $sqlInsert = substr_replace($sqlInsert, ';', -1, 1);
-                        $band = $claReporte->obtenerReportesGral($sqlInsert);
-                        // echo "<br> -***** -".$band;
-                        if (!$band) {
-                            echo '<div class = "alert alert-danger" role = "alert">Hubo un error al insertar '
-                            . '<a class="btn btn-info btn-lg" onclick=""  href="./listadoReportesGral.php"><span class="glyphicon glyphicon-download-alt" aria-hidden="true" style="cursor: pointer; text-align: center"></span></a></div> ';
-                        } else {
-                            echo '<div class = "alert alert-success" role = "alert">El reporte Se modifico correctamente</div> ';
-                            // echo "<script>location.href='./listadoReportesGral.php'</script>";
-                            //echo "<script>$('.modal-backdrop.fade').hide(); console.log('pasoo')</script>";
+                        $deleteVal = $claReporte->obtenerReportesGral($sqlDelete);
+                        if ($deleteVal) {
+                            $sqlInsert = substr_replace($sqlInsert, ';', -1, 1);
+                            $band = $claReporte->obtenerReportesGral($sqlInsert);
+                            // echo "<br> -***** -".$band;
+                            if (!$band) {
+                                echo '<div class = "alert alert-danger" role = "alert">Hubo un error al insertar '
+                                . '<a class="btn btn-info btn-lg" onclick=""  href="./listadoReportesGral.php"><span class="glyphicon glyphicon-download-alt" aria-hidden="true" style="cursor: pointer; text-align: center"></span></a></div> ';
+                            } else {
+                                echo '<div class = "alert alert-success" role = "alert">El reporte Se modifico correctamente</div> ';
+                                // echo "<script>location.href='./listadoReportesGral.php'</script>";
+                                //echo "<script>$('.modal-backdrop.fade').hide(); console.log('pasoo')</script>";
+                            }
                         }
-                    }
-                } else {
-                    echo ' <div class = "alert alert-danger" role = "alert">
+                    } else {
+                        echo ' <div class = "alert alert-danger" role = "alert">
                     El reporte tiene problemas con los siguientes Documentos &nbsp;&nbsp;<a class="btn btn-info btn-lg" onclick="location.href="  href="#"><span class="glyphicon glyphicon-level-up" aria-hidden="true" style="cursor: pointer; text-align: center">&nbsp;Volver</span>Volver</a>
                     </div><h5>' . $valDoc . '</h5>';
-                    // echo "<p><b>**** El reporte tiene problemas con los siguientes Documentos  ****** </b></p> " . $valDoc;
+                        // echo "<p><b>**** El reporte tiene problemas con los siguientes Documentos  ****** </b></p> " . $valDoc;
+                    }
+                } else {
+                    echo '<div class = "alert alert-danger" role = "alert">El reporte excede el limite permitido de 5000 Registros</div> ';
                 }
                 ?>
             </div>

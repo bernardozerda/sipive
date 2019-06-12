c6089dcc485ee335df176477e915d8ebc6adb88f
Liliana <lbastoz@habitatbogota.gov.co>
2019-06-12 09:54:19 -0500

    Cambios en los permisos de visualizacion de tablero de proyectos

--- contenidos/proyectos/crm/indicadores.php
+++ contenidos/proyectos/crm/indicadores.php
@@ -9,18 +9,23 @@
 $arrGrupoPermitidos[6][1] = 1;
 $arrGrupoPermitidos[6][20] = 47;
 $arrGrupoPermitidos[6][20] = 46;
+$arrGrupoPermitidos[6][7] = 8;
+$arrGrupoPermitidos[6][7] = 49;
+
 
 $bolGrupoPermitido = false;
 
 $txtPlantilla = "";
 $arrProyTableroPal = "";
 $arrayProyDependencia = "";
+$seqProyecto = $_SESSION['seqProyecto'];
 
 foreach ($_SESSION["arrGrupos"][$seqProyecto] as $seqGrupo => $seqProyectoGrupo) {
     if (isset($arrGrupoPermitidos[$seqProyecto][$seqGrupo]) and ( $arrGrupoPermitidos[$seqProyecto][$seqGrupo] == $seqProyectoGrupo )) {
         $bolGrupoPermitido = true;
     }
 }
+
 if ($bolGrupoPermitido) {
     $arrayProyDependencia = Proyecto::obtenerDatosProyectosDependencia();
     $txtPlantilla = "proyectos/crm/tableroDependencia.tpl";
